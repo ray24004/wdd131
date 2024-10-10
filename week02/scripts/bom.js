45cedd12d9c9daaf4be55eaa2a9466b91efd24f9
ray24004 <ray24004@byui.edu>
2024-10-09 21:57:01 -0300

    DOM Events ponder activity

--- week02/scripts/bom.js
+++ week02/scripts/bom.js
@@ -10,3 +10,20 @@ deleteButton.textContent = "❌";
 
 listItem.append(deleteButton);
 listElement.append(listItem);
+
+
+
+function copyInput(event) {
+    console.log(event)
+}
+buttonElement.addEventListener("click", copyInput);
+
+
+document.addEventListener("keydown", logKey);
+
+function logKey(e) {
+  // how do we know which key was pressed?
+  console.log(e);
+  // checkout e.code, e.key, and e.keyCode
+  // what is the difference?
+}
