4519ff75c269169d34b05a71c061a383ac5448c6
ray24004 <ray24004@byui.edu>
2024-10-30 08:06:40 -0300

    Dynamically fill page content

---
+++ project/scripts/index.js
@@ -0,0 +1,79 @@
+const professional = {
+    name: "Wesley Raymundo",
+    title: "Game Developer",
+    email: "contact@wesleyraymundo.dev",
+    phone: "+55 41 99672 2591",
+    location: "Colombo, Paraná, Brazil",
+    linkedin: "wesley-raymundo"
+}
+fillProfessionalLabels(professional);
+
+const games = [
+    {
+        title: "Game 1",
+        access: "",
+        genre: "",
+        company: "",
+        outcomes: [
+            "Outcome 1",
+            "Outcome 2",
+            "Outcome 3"
+        ],
+        callActionLabel: "Watch Gameplay",
+        callActionUrl: "#",
+        imageUrl: ""
+    }
+]
+fillGamesSection(games);
+
+function fillProfessionalLabels(professionalData) {
+    const nameLabels = document.querySelectorAll(".professionalName");
+    const titleLabels = document.querySelectorAll(".professionalTitle");
+    const emailLabels = document.querySelectorAll(".professionalEmail");
+    const phoneLabels = document.querySelectorAll(".professionalPhone");
+    const locationLabels = document.querySelectorAll(".professionalLocation");
+
+    nameLabels.forEach(l => l.innerHTML = professionalData.name);
+    titleLabels.forEach(l => l.innerHTML = professionalData.title);
+    emailLabels.forEach(l => l.innerHTML = professionalData.email);
+    phoneLabels.forEach(l => l.innerHTML = professionalData.phone);
+    locationLabels.forEach(l => l.innerHTML = professionalData.location);
+
+    const linkedInAnchors = document.querySelectorAll("a.professionalLinkedIn");
+    linkedInAnchors.forEach(a => a.setAttribute("href", `https://www.linkedin.com/in/${professionalData.linkedin}`))
+}
+
+function fillGamesSection(gameDataArray) {
+    const gamesSection = document.querySelector("#gamesSection");
+    gameDataArray.forEach(g => {
+        const article = document.createElement("article");
+        article.classList.add("gameArticle")
+    
+        let outcomeItems = "";
+        g.outcomes.forEach(o => {
+            outcomeItems += `
+                <li>${o}</li>
+            `;
+        });
+    
+        article.innerHTML = `
+            <article class="gameArticle">
+                <picture>
+                    <img src="${g.imageUrl}" alt="${g.title} image">
+                </picture>
+                <h2>${g.title}</h2>
+                <p>[ ${g.access} ]</p>
+                <p>${g.genre}</p>
+                <p>- ${g.company} -</p>
+                <p>Outcomes</p>
+                <ul>
+                    ${outcomeItems}
+                </ul>
+                <a href="${g.callActionUrl}" class="gameCallAction">${g.callActionLabel}</a>
+            </article>
+        `;
+    
+        gamesSection.appendChild(article);
+    })
+}
+
